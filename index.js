ed440fb1f29bbfede64d4470e5254496c6db04b6
Michael Losada <michael.losada@gmail.com>
2018-02-14 18:41:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function divide(a, b){
   return a / b
 }
 
-
+function inc(a){
+  return a += 1
+}
 
 it('inc(n) increments n and returns the result', function() {
   expect(inc(a)).toEqual(a + 1)
